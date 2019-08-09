2166afde0a549b285b26a683992c55e092cdd682
HERHAR Fares <herhar.Fares@yandex.com>
2019-08-09 19:28:03 +0100

    Changing the structure

--- __init__.py
+++ __init__.py
@@ -1,86 +1,37 @@
-from items import *
-from tools import *
-import time
-import os
-import res.ouedkniss.ouedkniss as oks
+from ouedkniss import *
 
 
-def fetch(cat, wl, skip=-1):
-	counter = 0
+SEARCH_LINK = "https://www.ouedkniss.com/boutiques/stores.php?store=&c={}&wilaya={}"
+CONTACT_LINK = "https://www.ouedkniss.com/store/contact.php?id={}"
 
-	# init the process
-	page_counter = 1
-	out = False
+
+def find_store_id(page_link):
+	"""
+	To extract the idS of the stores.
+	:param link:
+	:return:
+	"""
+	html = requests.get(page_link, stream=True)
+	html = BeautifulSoup(html.text, 'html.parser')
 	
-	while out is False:
-		# make the search link
-		link = SEARCH_LINK_PAGE.format(cat, wl, page_counter)
+	links = html.find_all("a", class_="store_name")
+
+	for link in links:
+		yield re.findall(r'id=(\d*)', link["href"])[0]
+
+def find_store_info(page_link):
+	"""
+	To extract the idS of the stores.
+	:param link:
+	:return:
+	"""
+	html = requests.get(page_link, stream=True)
+	html = BeautifulSoup(html.text, 'html.parser')
 	
-		# the links
-		links = list(oks.getLinks(link))
-		# in this case the page is not valid
-		# so we go to next wilaya
-		if len(links) != 0:
-			for an_link in links:
-				if counter <= skip:
-					print(counter, "Page::", page_counter)
-					counter += 1
-					continue
-				# get the image link
-				img_link = oks.getImgLink(an_link)
-					
-				# if the link exists, download the image
-				if len(img_link) != 0:
+	links = html.find("div", {"id": "informations"})
 
-					# download the image
-					downloadImg(img_link, "cache/" + cat + "/" +
-					            str(wilayaSearch.index(wl) + 1)+"/phone" + str(counter))
+	print(links)
 
-					print(counter, "Page::", page_counter, "Wilaya::", wl)
-					counter += 1
-				else:
-					continue
-		else:
-			out = True
-			continue
-				
-		# access the next page
-		page_counter += 1
-
-
-def scritch():
-	file = open("/home/fares/Desktop/data.txt", 'a')
-	
-	for cat in category:
-	 	for wl in range(1, 49):
-	 		current_dir = "/home/fares/Music/phones/cache/" + cat + "/" + str(wl) + "/"
-	 		l = os.listdir(current_dir)
-	 		os.chdir(current_dir)
-
-	 		for number in l:
-			 	try:
-			 		numbers = imgToText(number)
-			 		if numbers != "":
-				 		numbers = numbers.split("/")
-
-				 		for single in numbers:
-				 			if len(single) > 1:
-				 				for num in single.split("\n"):
-				 					num = num.replace(" ", "")
-				 					num = uniTel(num, "213")
-				 					
-				 					print("["+'"'+cat+'"'+","+'"'+wilaya[wl-1]+'"'+","+'"'+num+'"'+"]")
-			 						
-			 						file.write("["+'"'+cat+'"'+","+'"'+wilaya[wl-1]+'"'+","+'"'+num+'"'+"]")
-			 						file.write("\n")
-			 	except Exception as e:
-			 		pass
-
-	file.close()
-
-if __name__ == "__main__":
-
-	scritch()
-
-
-	
+if __name__ == '__main__':
+	pass
+	find_store_info("https://www.ouedkniss.com/store/contact.php?id=6275")
--- notes.py
+++ notes.py
@@ -8,25 +8,20 @@ will be added into the file.
 
 # declaration of the tables I created.
 """
-CREATE TABLE IF NOT EXISTS Wilaya(
-	code varchar(50),
-	name varchar(50),
-	
-	PRIMARY KEY code
+CREATE TABLE IF NOT EXISTS wilaya(
+	code varchar(50) PRIMARY KEY,
+	name varchar(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Category(
-	name varchar(50),
-	
-	PRIMARY KEY name
+CREATE TABLE IF NOT EXISTS category(
+	name varchar(50) PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS Phone(
-	number varchar(50),
-	category varchar(50),
-	wilaya varchar(50),
+CREATE TABLE IF NOT EXISTS phone(
+	number varchar(50) PRIMARY KEY,
+	category varchar(50) NOT NULL,
+	wilaya varchar(50) NOT NULL,
 	
-	PRIMARY KEY number,
 	FOREIGN KEY (wilaya) REFERENCES Wilaya(code),
 	FOREIGN KEY (category) REFERENCES Category(name)
 );
@@ -35,6 +30,31 @@ CREATE TABLE IF NOT EXISTS Phone(
 
 
 """
+query = " INSERT INTO wilaya(code,name) VALUES(?,?)"
+print("QUERY PREPARED:: ", db.prepareQuery(query))
+for wl in range(1, 49):
+	print(db.insertRow([str(wl), wilaya[wl - 1]]))
 
+query = " INSERT INTO category(name) VALUES(?)"
+print("QUERY PREPARED:: ", db.prepareQuery(query))
+for cat in category:
+	print(db.insertRow([cat]))
+
+	query = " INSERT INTO phone(number,category,wilaya) VALUES(?,?,?)"
+
+	print("DATA BASE OPENED:: ", db.connect())
+	print("QUERY PREPARED:: ", db.prepareQuery(query))
+	print("\n")
 	
+	file = open("data.txt", "r")
+	while True:
+		data = file.readline()
+
+		if data == "":
+			break
+		else:
+			data = data[1:-2].split(",")
+			to_insert = [data[-1]]
+			to_insert += data[:-1] 
+			print(to_insert, db.insertRow(to_insert))
 """
