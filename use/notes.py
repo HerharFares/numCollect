e45ae266ccce87096342b4a5f8c07b97fd7a5f88
HERHAR Fares <herhar.Fares@yandex.com>
2020-01-15 01:39:20 +0100

    Wed Jan 15 01:39:20 CET 2020

--- use/notes.py
+++ use/notes.py
@@ -26,35 +26,3 @@ CREATE TABLE IF NOT EXISTS phone(
 	FOREIGN KEY (category) REFERENCES Category(name)
 );
 """
-
-
-
-"""
-query = " INSERT INTO wilaya(code,name) VALUES(?,?)"
-print("QUERY PREPARED:: ", db.prepareQuery(query))
-for wl in range(1, 49):
-	print(db.insertRow([str(wl), wilaya[wl - 1]]))
-
-query = " INSERT INTO category(name) VALUES(?)"
-print("QUERY PREPARED:: ", db.prepareQuery(query))
-for cat in category:
-	print(db.insertRow([cat]))
-
-	query = " INSERT INTO phone(number,category,wilaya) VALUES(?,?,?)"
-
-	print("DATA BASE OPENED:: ", db.connect())
-	print("QUERY PREPARED:: ", db.prepareQuery(query))
-	print("\n")
-	
-	file = open("data.txt", "r")
-	while True:
-		data = file.readline()
-
-		if data == "":
-			break
-		else:
-			data = data[1:-2].split(",")
-			to_insert = [data[-1]]
-			to_insert += data[:-1] 
-			print(to_insert, db.insertRow(to_insert))
-"""
