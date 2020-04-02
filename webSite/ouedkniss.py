40b6a3ea78270cc29d33d211598ae0caef3a1c99
HERHAR Fares <herhar.Fares@yandex.com>
2020-04-02 14:44:08 +0100

    last commit

--- webSite/ouedkniss.py
+++ webSite/ouedkniss.py
@@ -1,5 +1,6 @@
 import re
 import os
+import requests
 from bs4 import BeautifulSoup
 
 from use.tools import *
@@ -14,170 +15,179 @@ SEARCH_LINK_PAGE = "https://www.ouedkniss.com/annonces/?c={}&wilaya=%2C{}&p={}"
 
 # The wilaya's(means states, thats how we name it in Algeria)
 wilaya = [
-	"Adrar", "Chlef", "Laghouat", "Oum El Bouaghi", "Batna", "Bejaia", "Biskra",
-	"Béchar", "Blida", "Bouira", "Tamanrasset", "Tébessa", "Tlemcen", "Tiaret",
+    "Adrar", "Chlef", "Laghouat", "Oum El Bouaghi", "Batna", "Bejaia", "Biskra",
+    "Béchar", "Blida", "Bouira", "Tamanrasset", "Tébessa", "Tlemcen", "Tiaret",
     "Tizi Ouzou", "Alger", "Djelfa", "Jijel", "Sétif", "Saïda", "Skikda",
     "Sidi Bel Abbes", "Annaba", "Guelma", "Constantine", "Medea", "Mostaganem", "M'Sila",
     "Mascara", "Ouargla", "Oran", "El Bayadh", "Illizi", "Bordj Bou Arreridj", "Boumerdes",
     "El Taref", "Tindouf", "Tissemsilt", "El Oued", "Khenchela", "Souk Ahras", "Tipaza",
-	"Mila", "Aïn Defla", "Naama", "Aïn Temouchent", "Ghardaia", "Relizane"]
+    "Mila", "Aïn Defla", "Naama", "Aïn Temouchent", "Ghardaia", "Relizane"]
 
 # The wilaya's(means states, that is how we name it in Algeria)
 # this form supports
 wilayaSearch = [
-	"Adrar", "Chlef", "Laghouat", "Oum+El+Bouaghi", "Batna", "Bejaia", "Biskra",
-	"Bechar", "Blida", "Bouira", "Tamanrasset", "Tebessa", "Tlemcen", "Tiaret",
+    "Adrar", "Chlef", "Laghouat", "Oum+El+Bouaghi", "Batna", "Bejaia", "Biskra",
+    "Bechar", "Blida", "Bouira", "Tamanrasset", "Tebessa", "Tlemcen", "Tiaret",
     "Tizi+Ouzou", "Alger", "Djelfa", "Jijel", "Setif", "Saida", "Skikda",
     "Sidi+Bel+Abbes", "Annaba", "Guelma", "Constantine", "Medea", "Mostaganem", "MSila",
     "Mascara", "Ouargla", "Oran", "El+Bayadh", "Illizi", "Bordj+Bou+Arreridj", "Boumerdes",
     "El+Taref", "Tindouf", "Tissemsilt", "El+Oued", "Khenchela", "Souk+Ahras", "Tipaza",
-	"Mila", "Ain+Defla", "Naama", "Ain+Temouchent", "Ghardaia", "Relizane"]
+    "Mila", "Ain+Defla", "Naama", "Ain+Temouchent", "Ghardaia", "Relizane"]
 
 
 # the categories
 # "materiaux_equipement", "voyage", "services", "boutiques"
 category = [
-	"telephones", "automobiles", "immobilier", "electronique",
-	"informatique", "vetements", "maison", "loisirs_divertissements",
-	"divers", "materiaux_equipement", "voyages", "services"
+    "telephones", "automobiles", "immobilier", "electronique",
+    "informatique", "vetements", "maison", "loisirs_divertissements",
+    "divers", "materiaux_equipement", "voyages", "services"
 ]
 ######################################################################
 ######################################################################
+
+
 def getImgLink(page_link):
-	"""
-	To extract the link that directs us to the
-	phone number's image, so we can download it
-	later, and extract the phone number from it.
-	:param page_link:
-	:return:
-	"""
-	html = requests.get(str(page_link), stream=True)
-	html = BeautifulSoup(html.text, 'html.parser')
-	
-	phone_link = html.find_all("img", alt="phone")
-	phone_link = re.findall(r'//(.*\.jpg)', str(phone_link))
-	
-	if len(phone_link) != 0:
-		phone_link = phone_link[0]
-		return "https://" + str(phone_link)
-	
-	else:
-		return ""
-	
-	
+    """
+    To extract the link that directs us to the
+    phone number's image, so we can download it
+    later, and extract the phone number from it.
+    :param page_link:
+    :return:
+    """
+    html = requests.get(str(page_link), stream=True)
+    html = BeautifulSoup(html.text, 'html.parser')
+
+    phone_link = html.find_all("img", alt="phone")
+    phone_link = re.findall(r'//(.*\.jpg)', str(phone_link))
+
+    if len(phone_link) != 0:
+        phone_link = phone_link[0]
+        return "https://" + str(phone_link)
+
+    else:
+        return ""
+
+
 def getLinks(link):
-	"""
-	To extract the links that directs us to the
-	the pages, so we can download the images that
-	contains the phone numbers.
-	:param link:
-	:return:
-	"""
-	html = requests.get(link)
-	html = BeautifulSoup(html.text, 'html.parser')
-	
-	a_link = html.find_all("li", class_="annonce_titre")
-	
-	for go in re.findall(r'href="(.*)" it', str(a_link)):
-		yield "https://ouedkniss.com/" + go
+    """
+    To extract the links that directs us to the
+    the pages, so we can download the images that
+    contains the phone numbers.
+    :param link:
+    :return:
+    """
+    html = requests.get(link)
+    html = BeautifulSoup(html.text, 'html.parser')
+
+    a_link = html.find_all("li", class_="annonce_titre")
+
+    for go in re.findall(r'href="(.*)" it', str(a_link)):
+        yield "https://ouedkniss.com/" + go
 ######################################################################
 ######################################################################
+
+
 def fetch(cat, wl, skip=-1):
-	counter = 0
+    counter = 0
 
-	# init the process
-	page_counter = 1
-	out = False
-	
-	while out is False:
-		# make the search link
-		link = SEARCH_LINK_PAGE.format(cat, wl, page_counter)
-	
-		# the links
-		links = list(getLinks(link))
-		# in this case the page is not valid
-		# so we go to next wilaya
-		if len(links) != 0:
-			try:
-				for an_link in links:
-					if counter <= skip:
-						print(counter, "Page::", page_counter)
-						counter += 1
-						continue
-					# get the image link
-					img_link = getImgLink(an_link)
-						
-					# if the link exists, download the image
-					if len(img_link) != 0:
+    # init the process
+    page_counter = 1
+    out = False
 
-						# download the image
-						downloadImg(img_link, "cache/" + cat + "/" +
-						            str(wilayaSearch.index(wl) + 1)+"/phone" + str(counter))
+    while out is False:
+        # make the search link
+        link = SEARCH_LINK_PAGE.format(cat, wl, page_counter)
 
-						print(counter, "Page::", page_counter, "Wilaya::", wl, "Category::", cat)
-						counter += 1
-					else:
-						continue
-			except Exception as e:
-				#raise e
-				pass
-		else:
-			out = True
-			continue
-				
-		# access the next page
-		page_counter += 1
+        # the links
+        links = list(getLinks(link))
+        # in this case the page is not valid
+        # so we go to next wilaya
+        if len(links) != 0:
+            try:
+                for an_link in links:
+                    if counter <= skip:
+                        print(counter, "Page::", page_counter)
+                        counter += 1
+                        continue
+                    # get the image link
+                    img_link = getImgLink(an_link)
+
+                    # if the link exists, download the image
+                    if len(img_link) != 0:
+
+                        # download the image
+                        downloadImg(img_link, "cache/" + cat + "/" +
+                                    str(wilayaSearch.index(wl) + 1)+"/phone" + str(counter))
+
+                        print(counter, "Page::", page_counter,
+                              "Wilaya::", wl, "Category::", cat)
+                        counter += 1
+                    else:
+                        continue
+            except Exception as e:
+                #raise e
+                pass
+        else:
+            out = True
+            continue
+
+        # access the next page
+        page_counter += 1
 
 
 def scritch():
-	file = open("/home/fares/Desktop/data_.txt", 'a')
-	
-	for cat in category:
-	 	for wl in range(16, 17):
-	 		current_dir = "/home/fares/Music/phones/cache/" + cat + "/" + str(wl) + "/"
-	 		l = os.listdir(current_dir)
-	 		os.chdir(current_dir)
+    file = open("/home/fares/Desktop/data_.txt", 'a')
 
-	 		for number in l:
-			 	try:
-			 		numbers = imgToText(number)
-			 		if numbers != "":
-				 		numbers = numbers.split("/")
+    for cat in category:
+        for wl in range(16, 17):
+            current_dir = "/home/fares/Music/phones/cache/" + \
+                cat + "/" + str(wl) + "/"
+            l = os.listdir(current_dir)
+            os.chdir(current_dir)
 
-				 		for single in numbers:
-				 			if len(single) > 1:
-				 				for num in single.split("\n"):
-				 					num = num.replace(" ", "")
-				 					num = uniTel(num, "213")
-				 					
-				 					print("["+'"'+cat+'"'+","+'"'+wilaya[wl-1]+'"'+","+'"'+num+'"'+"]")
-			 						
-			 						file.write("["+'"'+cat+'"'+","+'"'+wilaya[wl-1]+'"'+","+'"'+num+'"'+"]")
-			 						file.write("\n")
-			 	except Exception as e:
-			 		pass
+            for number in l:
+                try:
+                    numbers = imgToText(number)
+                    if numbers != "":
+                        numbers = numbers.split("/")
 
-	file.close()
+                        for single in numbers:
+                            if len(single) > 1:
+                                for num in single.split("\n"):
+                                    num = num.replace(" ", "")
+                                    num = uniTel(num, "213")
+
+                                    print(
+                                        "["+'"'+cat+'"'+","+'"'+wilaya[wl-1]+'"'+","+'"'+num+'"'+"]")
+
+                                    file.write(
+                                        "["+'"'+cat+'"'+","+'"'+wilaya[wl-1]+'"'+","+'"'+num+'"'+"]")
+                                    file.write("\n")
+                except Exception as e:
+                    pass
+
+    file.close()
+
 
 def write_to_base():
-	query = " INSERT INTO phone(number,category,wilaya) VALUES(?,?,?)"
-	db = Db("dataAr/data.db")
+    query = " INSERT INTO phone(number,category,wilaya) VALUES(?,?,?)"
+    db = Db("dataAr/data.db")
 
-	print("DATA BASE OPENED:: ", db.connect())
-	print("QUERY PREPARED:: ", db.prepareQuery(query))
-	print("\n")
-	
-	file = open("/home/fares/Desktop/data_.txt", "r")
-	while True:
-		data = file.readline()
+    print("DATA BASE OPENED:: ", db.connect())
+    print("QUERY PREPARED:: ", db.prepareQuery(query))
+    print("\n")
 
-		if data == "":
-			break
-		else:
-			data = data[1:-2].split(",")
-			to_insert = [data[-1]]
-			to_insert += data[:-1] 
-			print(to_insert, db.insertRow(to_insert))
+    file = open("/home/fares/Desktop/data_.txt", "r")
+    while True:
+        data = file.readline()
 
-	db.close()
-#187826
+        if data == "":
+            break
+        else:
+            data = data[1:-2].split(",")
+            to_insert = [data[-1]]
+            to_insert += data[:-1]
+            print(to_insert, db.insertRow(to_insert))
+
+    db.close()
+# 187826
--- webSite/vivastreet.py
+++ webSite/vivastreet.py
@@ -8,92 +8,94 @@ GLOABL_PAGE = ""
 SEARCH_LINK = "https://search.vivastreet.com/{}/fr/t+{}"
 
 cat = ["autos-motos-bateaux", "bebes-puericulture", "engins-materiels-pro", "chien-chat",
-"tous-autres", "vente-maison", "vente-multimedia", "vente-loisirs", "annonces-immobilier"]
+       "tous-autres", "vente-maison", "vente-multimedia", "vente-loisirs", "annonces-immobilier"]
+
 
 def getNum(page_link):
-	"""
-	To extract the phone number, and the region
-	"""
-	html = requests.get(str(page_link), stream=True)
-	html = BeautifulSoup(html.text, 'html.parser')
-	
-	phone = html.find("span", {"class":"phone_link"})
-	phone = re.findall(r'data-phone-number="([0-9]*)"', str(phone))
-	
-	region = html.find("div", {"class":"kiwii-no-link-color kiwii-no-link-decoration"})
-	region = re.findall(r'<div class="kiwii-no-link-color kiwii-no-link-decoration">(.*)<br/>.*<br/>.*</div>', str(region))
-	
-	try:		
-		region = region[0].split("<br/>")
-		return [phone[0], region[0]]
-	except Exception as e:
-		print("----------")
-		return None
+    """
+    To extract the phone number, and the region
+    """
+    html = requests.get(str(page_link), stream=True)
+    html = BeautifulSoup(html.text, 'html.parser')
+
+    phone = html.find("span", {"class": "phone_link"})
+    phone = re.findall(r'data-phone-number="([0-9]*)"', str(phone))
+
+    region = html.find(
+        "div", {"class": "kiwii-no-link-color kiwii-no-link-decoration"})
+    region = re.findall(
+        r'<div class="kiwii-no-link-color kiwii-no-link-decoration">(.*)<br/>.*<br/>.*</div>', str(region))
+
+    try:
+        region = region[0].split("<br/>")
+        return [phone[0], region[0]]
+    except Exception as e:
+        print("----------")
+        return None
 
 
 def getLinks(link):
-	"""
-	To extract the links that directs us to the
-	the pages, so we can download the images that
-	contains the phone numbers.
-	:param link:
-	:return:
-	"""
-	global GLOABL_PAGE
-	
-	html = requests.get(link)
-	html = BeautifulSoup(html.text, 'html.parser')
+    """
+    To extract the links that directs us to the
+    the pages, so we can download the images that
+    contains the phone numbers.
+    :param link:
+    :return:
+    """
+    global GLOABL_PAGE
 
-	GLOABL_PAGE = html.text
-	
-	a_link = html.find_all("a", {"class":"clad__wrapper"}, href=True)
+    html = requests.get(link)
+    html = BeautifulSoup(html.text, 'html.parser')
 
-	for l in a_link:
-		yield l["href"]
+    GLOABL_PAGE = html.text
 
+    a_link = html.find_all("a", {"class": "clad__wrapper"}, href=True)
+
+    for l in a_link:
+        yield l["href"]
 
 
 def fetch(cat, skip=-1):
-	file = open(cat + ".txt", "a")
+    file = open(cat + ".txt", "a")
 
-	counter = 0
+    counter = 0
 
-	# init the process
-	page_counter = 1
-	out = False
+    # init the process
+    page_counter = 1
+    out = False
 
-	while out is False:
-		# make the search link
-		search_link = SEARCH_LINK.format(cat, page_counter)
+    while out is False:
+        # make the search link
+        search_link = SEARCH_LINK.format(cat, page_counter)
 
-		while True:
-			try: 
-				for link in getLinks(search_link):
-					if counter <= skip:
-						print(counter, "Page::", page_counter)
-						counter += 1
-						continue
+        while True:
+            try:
+                for link in getLinks(search_link):
+                    if counter <= skip:
+                        print(counter, "Page::", page_counter)
+                        counter += 1
+                        continue
 
-					num = getNum(link)
+                    num = getNum(link)
 
-					if num is not None:
-						file.write(str(num))
-						file.write("\n")
-						print(counter, "Page::", page_counter, "Category::", cat)
-						# print(str(num))
-						counter += 1
-				break
-			except:
-				pass
-			
-		if "Suivante" not in GLOABL_PAGE:
-			out = True
-		else:
-			page_counter += 1
+                    if num is not None:
+                        file.write(str(num))
+                        file.write("\n")
+                        print(counter, "Page::", page_counter, "Category::", cat)
+                        # print(str(num))
+                        counter += 1
+                break
+            except:
+                pass
 
-	file.close()
+        if "Suivante" not in GLOABL_PAGE:
+            out = True
+        else:
+            page_counter += 1
+
+    file.close()
 
 
 if __name__ == '__main__':
-	#fetch("vente-loisirs")
-	pass
+    # fetch("vente-loisirs")
+    pass
